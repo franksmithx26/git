0898eebee4e3583491a827e2ac2bb3f85a7e66d0
franksmithx26 <110610189+franksmithx26@users.noreply.github.com>
2022-08-04 20:38:53 +0800

    Initial commit

---
+++ wp-content/plugins/download-monitor/src/Shop/Admin/ProductTableColumns.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Never5\DownloadMonitor\Shop\Admin;
+
+use Never5\DownloadMonitor\Shop\Services\Services;
+use Never5\DownloadMonitor\Shop\Util\PostType;
+
+class ProductTableColumns {
+
+	/**
+	 * Setup product columns
+	 */
+	public function setup() {
+		add_filter( 'manage_edit-' . PostType::KEY . '_columns', array( $this, 'add_columns' ) );
+		add_action( 'manage_' . PostType::KEY . '_posts_custom_column', array( $this, 'column_data' ), 10, 2 );
+		add_filter( 'manage_edit-' . PostType::KEY . '_sortable_columns', array( $this, 'sortable_columns' ) );
+	}
+
+	/**
+	 * columns function.
+	 *
+	 * @access public
+	 *
+	 * @param array $columns
+	 *
+	 * @return array
+	 */
+	public function add_columns( $columns ) {
+		$columns = array();
+
+		$columns["cb"]        = "<input type=\"checkbox\" />";
+		$columns["thumb"]     = '<span>' . __( "Image", 'download-monitor' ) . '</span>';
+		$columns["title"]     = __( "Title", 'download-monitor' );
+		$columns["shortcode"] = __( "Shortcode", 'download-monitor' );
+		$columns["price"]     = __( "Price", 'download-monitor' );
+		$columns["date"]      = __( "Date", 'download-monitor' );
+
+		return $columns;
+	}
+
+	/**
+	 * custom_columns function.
+	 *
+	 * @access public
+	 *
+	 * @param string $column
+	 * @param int $post_id
+	 *
+	 * @return void
+	 */
+	public function column_data( $column, $post_id ) {
+
+		/** @var \Never5\DownloadMonitor\Shop\Product\Product $product */
+		try {
+			$product = Services::get()->service( 'product_repository' )->retrieve_single( $post_id );
+		} catch ( \Exception $exception ) {
+			return;
+		}
+
+		switch ( $column ) {
+			case "thumb" :
+				echo wp_kses_post( $product->get_image() );
+				break;
+			case "price" :
+				echo esc_html( dlm_format_money( $product->get_price() ) );
+				break;
+			case "shortcode" :
+				echo '<button class="wpchill-tooltip-button copy-dlm-shortcode button button-primary dashicons dashicons-shortcode" style="width:40px;"><div class="wpchill-tooltip-content"><span class="dlm-copy-text">' . esc_html__( 'Copy shortcode', 'download-monitor' ) . '</span><div class="dl-shortcode-copy"><code>[dlm_buy id="' . absint( $post_id ) . '"]</code><input type="text" value="[dlm_buy id=\'' . absint( $post_id ) . '\']" class="hidden"></div></div></button>';
+				break;
+		}
+	}
+
+	/**
+	 * sortable_columns function.
+	 *
+	 * @access public
+	 *
+	 * @param mixed $columns
+	 *
+	 * @return array
+	 */
+	public function sortable_columns( $columns ) {
+		$custom = array(
+			'price' => 'price'
+		);
+
+		return wp_parse_args( $custom, $columns );
+	}
+}
